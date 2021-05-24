e3a8cf88b15a5923d34a1896d202a04bcf2fd46a
Caio Chami <caio.chami@puzl.place>
2021-05-23 22:13:14 -0300

    wip: Checkbox component

---
+++ src/components/elements/RaisedButton.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button } from "react-native-elements";
+
+const RaisedButton = (props) => <Button raised {...props} />;
+
+export default RaisedButton;
---
+++ src/utils/Color.js
@@ -0,0 +1,8 @@
+export default {
+  warning: "#910dcf",
+  secondary: "#121456",
+  primary: "#fff",
+  muted: "#656eab",
+  background: "#434aa0",
+  light: "#a4b1fe",
+};
